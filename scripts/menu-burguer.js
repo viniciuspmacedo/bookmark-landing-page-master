cc0b6a4ead420c404462daca222269daa6f5379a
viniciuspmacedo <viniciusp.macedo@gmail.com>
2024-03-24 11:35:15 -0300

    feat: implementa menu hamburger

---
+++ scripts/menu-burguer.js
@@ -0,0 +1,16 @@
+// declara as referencias aos botões
+const menuBurguerBtn = document.querySelector('.burguer-button')
+const closeBtn = document.querySelector('.close-button')
+
+// declara os elementos manipulados
+const navbar = document.querySelector('.header__navbar')
+
+menuBurguerBtn.addEventListener('click', () => {
+    navbar.setAttribute('style', 'transform:translateX(0%);')
+    menuBurguerBtn.setAttribute('style','display:none')
+})
+
+closeBtn.addEventListener('click', () => {
+    navbar.setAttribute('style', 'transform:translateX(100%)')
+    menuBurguerBtn.setAttribute('style','display:block')
+})
